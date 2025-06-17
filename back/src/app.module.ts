e814eb5779d1da5c2cd2016683574555a10d5f40
Nass10000 <nassimwhazim@gmail.com>
2025-06-17 12:17:35 -0400

    actualizacion

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,44 +4,36 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
 import { CommentsModule } from './comments/comments.module';
-
 import { UsersModule } from './users/users.module';
-
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host:       config.get('DB_HOST'),
-        port:       config.get<number>('DB_PORT'),
-        username:   config.get('DB_USERNAME'),
-        password:   config.get('DB_PASSWORD'),
-        database:   config.get('DB_NAME'),
-        ssl: {
-          rejectUnauthorized: false
-        },
-        entities:   [__dirname + '/**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/*{.ts,.js}'],
-        synchronize: true,
-        dropSchema: false,
-      }),
+      useFactory: (config: ConfigService) => {
+        const url = config.get<string>('DATABASE_URL');
+        return {
+          type: 'postgres',
+          url,
+          ssl: { rejectUnauthorized: false },
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migrations/*{.ts,.js}'],
+          synchronize: true,
+        };
+      },
       inject: [ConfigService],
     }),
     AuthModule,
     UsersModule,
     MoviesModule,
     CommentsModule,
-
   ],
 })
-
 export class AppModule {
   constructor() {
     console.log('✅ AppModule cargado');
-    console.log('🔑 DB_HOST:', process.env.DB_HOST);
+    console.log('🔑 DATABASE_URL:', process.env.DATABASE_URL);
     console.log('🔑 AUTH0_DOMAIN:', process.env.AUTH0_DOMAIN);
     console.log('🔑 AUTH0_CLIENT_ID:', process.env.AUTH0_CLIENT_ID);
     console.log('🔑 AUTH0_CALLBACK_URL:', process.env.AUTH0_CALLBACK_URL);
