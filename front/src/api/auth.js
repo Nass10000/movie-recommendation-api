60363317c42fddc7bf2bbf0a824a7f4c41735869
Nass10000 <nassimwhazim@gmail.com>
2025-06-17 13:38:47 -0400

    actualizacion

--- front/src/api/auth.js
+++ front/src/api/auth.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export async function register(data) {
   console.log('POST /auth/register', data);
