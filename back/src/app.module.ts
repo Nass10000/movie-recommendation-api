fb61cdc02226c28944db0709fec93edfc2eca204
Nass10000 <nassimwhazim@gmail.com>
2025-06-17 11:55:30 -0400

    actualizacion

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -13,14 +13,17 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        host:       config.get('DB_HOST'),
+        port:       config.get<number>('DB_PORT'),
+        username:   config.get('DB_USERNAME'),
+        password:   config.get('DB_PASSWORD'),
+        database:   config.get('DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false
+        },
+        entities:   [__dirname + '/**/*.entity{.ts,.js}'],
         migrations: [__dirname + '/migrations/*{.ts,.js}'],
         synchronize: true,
         dropSchema: false,
