07969459f37c2618f323fafa9ea01fa737aafcc9
Nass10000 <nassimwhazim@gmail.com>
2025-06-17 12:43:38 -0400

    actualizacion

--- back/src/common/data-source.ts
+++ back/src/common/data-source.ts
@@ -5,13 +5,8 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT!, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: false },    
-
+  url: process.env.DATABASE_URL,      // usar la URL completa
+  ssl: { rejectUnauthorized: false }, // obligatoria en Render
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
 });
