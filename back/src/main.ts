b42fa4dda81bb77b96c2ec41cc4be177836cc12b
Nass10000 <nassimwhazim@gmail.com>
2025-06-18 16:18:07 -0400

    actualizacion

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,3 +1,5 @@
+console.log('Arrancando backend...');
+
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/all-exceptions.filter';
@@ -8,7 +10,7 @@ import { Request, Response, NextFunction } from 'express';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-// Importa los módulos que definen rutas para la documentación Swagger
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
@@ -21,23 +23,19 @@ async function bootstrap() {
   console.log('🟢 DATABASE_URL:', process.env.DATABASE_URL);
   console.log('==============================');
 
-  // Inicializa la conexión a la base de datos
+
   await AppDataSource.initialize();
 
-  // Crea la aplicación Nest
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Filtros y configuración inicial
+
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  // Sirve archivos estáticos del frontend (Vite build) desde carpeta 'front/dist'
-  // app.useStaticAssets(join(__dirname, '..', '..', 'front', 'dist'));
-  // console.log('🟢 Sirviendo estáticos desde', join(__dirname, '..', '..', 'front', 'dist'));
 
-  // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('Movie Recommendation API')
     .setDescription('API para recomendar películas')
@@ -54,20 +52,7 @@ async function bootstrap() {
     res.status(200).json(document),
   );
 
-  // --- SPA Fallback: Soluciona rutas directas a tu React app ---
-  // SOLO excluye rutas que DE VERDAD son solo API/documentación:
-  // const expressApp = app.getHttpAdapter().getInstance() as any;
-  // expressApp.get('*', (req: Request, res: Response, next: NextFunction) => {
-  //   if (
-  //     req.originalUrl.startsWith('/api') ||
-  //     req.originalUrl.startsWith('/swagger-json')
-  //   ) {
-  //     return next();
-  //   }
-  //   res.sendFile(join(__dirname, '..', '..', 'front', 'dist', 'index.html'));
-  // });
-
-  // Usa el puerto de la variable de entorno PORT o 3000 por defecto
+  
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
   console.log('==============================');
